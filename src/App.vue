43ceeb2c634e8e88113bbf7379267a098cb44b3f
Pedro Vinnícius Bernhard <pvbernhard@gmail.com>
2024-05-29 01:17:07 -0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+<div class="container">
+  <header class="text-center text-light my-4">
+    <h1 class="mb-4">✍️ Tarefas</h1>
+    <form class="add text-center my-4">
+      <label class="text-light">O que você quer fazer? 🤔</label>
+      <input class="form-control m-auto my-4" type="text" placeholder="ex.: aprender vue" name="add" />
+      <div class="text-center">
+        <input type="submit" class="btn btn-light" value="Adicionar tarefa" />
+      </div>
+    </form>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <ul class="list-group todos mx-auto text-light delete">
+    <li
+      class="list-group-item d-flex justify-content-between align-items-center"
+    >
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="check-1">
+        <label class="form-check-label" for="check-1">
+          Tarefa #1
+        </label>
+      </div>
+      <div class="actions">
+        <button class="btn btn-danger">Delete</button>
+      </div>
+    </li>
+  </ul>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
