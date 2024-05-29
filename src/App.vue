0895278403b573db624d29685cf3a09416466d6f
Pedro Vinnícius Bernhard <pvbernhard@gmail.com>
2024-05-29 03:31:17 -0300

    adicionar tarefa

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 
 const tarefas = ref([]);
+const novaTarefa = ref('');
 
 async function toggleTarefa(id) {
   const resultado = await fetch("http://localhost:8000/update/" + id)
@@ -13,9 +14,27 @@ async function toggleTarefa(id) {
   }
 }
 
-fetch("http://localhost:8000/")
-    .then(response => response.json())
-    .then(data => tarefas.value = data);
+async function adicionarTarefa(titulo) {
+  const resultado = await fetch("http://localhost:8000/add", {
+    method: 'POST',
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    body: `title=${titulo}`,
+  }).then(response => response.json());
+  
+  if (resultado) {
+    carregarTarefas();
+  }
+}
+
+function carregarTarefas() {
+  fetch("http://localhost:8000/")
+      .then(response => response.json())
+      .then(data => tarefas.value = data);
+}
+
+carregarTarefas()
 </script>
 
 <template>
@@ -24,9 +43,9 @@ fetch("http://localhost:8000/")
     <h1 class="mb-4">✍️ Tarefas</h1>
     <form class="add text-center my-4">
       <label class="text-light">O que você quer fazer? 🤔</label>
-      <input class="form-control m-auto my-4" type="text" placeholder="ex.: aprender vue" name="add" />
+      <input class="form-control m-auto my-4" v-model="novaTarefa" type="text" placeholder="ex.: aprender vue" name="add" />
       <div class="text-center">
-        <input type="submit" class="btn btn-light" value="Adicionar tarefa" />
+        <input type="submit" class="btn btn-light" value="Adicionar tarefa" @click.prevent="adicionarTarefa(novaTarefa)" />
       </div>
     </form>
   </header>
